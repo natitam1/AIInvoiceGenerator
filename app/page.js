de479624f88088ad0bc8acbfa8ef0f22ab9d558e
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-09-29 14:52:55 +0300

    chore: add state variables to control loading and messages array

--- app/page.js
+++ app/page.js
@@ -1,5 +1,9 @@
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
+  const [expand, setExpand] = useState(false);
+  const [messages, setMessages] = useState([]);
+  const [loading, setLoading] = useState(false);
   return <div>Home</div>;
 }
