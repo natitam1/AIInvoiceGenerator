b1780077d154275ed840785b52723f90cc3319f1
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-09-30 13:47:49 +0300

    style: add animation and smooth transition for the toggler

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,6 @@
 "use client";
 import { assets } from "@/assets/assets";
+import Sidebar from "@/components/Sidebar";
 import Image from "next/image";
 import { useState } from "react";
 
@@ -10,7 +11,7 @@ export default function Home() {
   return (
     <div>
       <div className="h-screen flex">
-        {/* -- scrollbar for md screen sizes -- */}
+        <Sidebar expand={expand} setExpand={setExpand} />
         <div className="flex-1 flex items-center justify-center flex-col px-4 pb-8 bg-[#292a2d] text-white relative">
           <div className="md:hidden absolute px-4 top-6 flex items-center justify-between w-full">
             <Image
--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -16,16 +16,35 @@ const Sidebar = ({ expand, setExpand }) => {
           }`}
         >
           <Image
+            alt=""
             src={expand ? assets.logo_text : assets.logo_icon}
             className={expand ? "w-36" : "w-10"}
           />
 
-          <div className="group relative flex items-center justify-center hover:bg-gray-500/20 transition-all duration-300 h-9 w-9 aspect-square rounded-lg cursor-pointer">
+          <div
+            onClick={() => (expand ? setExpand(false) : setExpand(true))}
+            className="group relative flex items-center justify-center hover:bg-gray-500/20 transition-all duration-300 h-9 w-9 aspect-square rounded-lg cursor-pointer"
+          >
             <Image src={assets.menu_icon} className="md:hidden" />
             <Image
+              alt=""
               src={expand ? assets.sidebar_close_icon : assets.sidebar_icon}
               className="hidden md:block w-7"
             />
+            <div
+              className={`absolute w-max ${
+                expand ? "left-1/2 -translate-x-1/2 top-12" : "-top-12 left-0"
+              } opacity-0 group-hover:opacity-100 transition-all bg-black text-white text-sm px-3 py-2 rounded-lg shadow-lg pointer-events-none`}
+            >
+              {expand ? "Close sidebar" : "Open sidebar"}
+              <div
+                className={`w-3 h-3 absolute bg-black rotate-45 ${
+                  expand
+                    ? "left-1/2 -top-1.5 -translate-x-1/2"
+                    : "left-4 -bottom-1.5"
+                }`}
+              ></div>
+            </div>
           </div>
         </div>
       </div>
