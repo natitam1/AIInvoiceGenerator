a2e62aa9314f33f40faaf73bd478a094632565c3
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-10-03 15:16:40 +0300

    feat: add a dashboard

--- frontend/invoice-generator/src/App.jsx
+++ frontend/invoice-generator/src/App.jsx
@@ -11,6 +11,10 @@ import { Toaster } from "react-hot-toast";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import ProtectedRoute from "./components/auth/ProtectedRoute";
+import AllInvoices from "./pages/Invoices/AllInvoices";
+import CreateInvoice from "./pages/Invoices/CreateInvoice";
+import InvoiceDetail from "./pages/Invoices/InvoiceDetail";
+import ProfilePage from "./pages/Profile/ProfilePage";
 
 const App = () => {
   return (
@@ -25,7 +29,7 @@ const App = () => {
           <Route path="/" element={<ProtectedRoute />}>
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="invoices" element={<AllInvoices />} />
-            <Route path="invoices/new" element={<CreateInvoices />} />
+            <Route path="invoices/new" element={<CreateInvoice />} />
             <Route path="invoices/:id" element={<InvoiceDetail />} />
             <Route path="profile" element={<ProfilePage />} />
           </Route>
--- frontend/invoice-generator/src/components/auth/ProtectedRoute.jsx
+++ frontend/invoice-generator/src/components/auth/ProtectedRoute.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { Navigate, Outlet } from "react-router-dom";
+import DashboardLayout from "./layout/DashboardLayout";
 
-const ProtectedRoute = () => {
-  return <div>ProtectedRoute</div>;
+const ProtectedRoute = ({ children }) => {
+  // Will integrate these values later
+  const isAuthenticated = true;
+  const isLoading = false;
+
+  if (isLoading) {
+    // You can render a spinner component here
+    return <div>Loading...</div>;
+  }
+
+  if (!isAuthenticated) {
+    <Navigate to="/login" replace />;
+  }
+  return <DashboardLayout>{children ? children : <Outlet />}</DashboardLayout>;
 };
 
 export default ProtectedRoute;
