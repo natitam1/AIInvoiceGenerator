21e0a8cbc773add81d0a74a780f1594f6bf70089
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-10-03 13:43:31 +0300

    chore: add public routes and catch all routes

--- frontend/invoice-generator/src/App.jsx
+++ frontend/invoice-generator/src/App.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { Navigate, Route, Router, Routes } from "react-router-dom";
+import SignUp from "./pages/Auth/SignUp";
+import Login from "./pages/Auth/Login";
 
 const App = () => {
-  return <div className="text-5xl">App</div>;
+  return (
+    <div>
+      <Router>
+        <Routes>
+          {/* Public Routes */}
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+
+          {/* Catch all route */}
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
