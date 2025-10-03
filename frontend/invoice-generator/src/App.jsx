822a604659ee895699dc2a5b4628a864c794354a
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-10-03 14:28:38 +0300

    chore: add toaster option

--- frontend/invoice-generator/src/App.jsx
+++ frontend/invoice-generator/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Navigate, Route, Router, Routes } from "react-router-dom";
 import SignUp from "./pages/Auth/SignUp";
 import Login from "./pages/Auth/Login";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
   return (
@@ -17,6 +18,14 @@ const App = () => {
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </Router>
+      <Toaster
+        toastOptions={{
+          className: "",
+          style: {
+            fontSize: "13px",
+          },
+        }}
+      />
     </div>
   );
 };
