c6b97d40ebce65722652e6a2d5944e81114fd21b
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-10-03 17:00:26 +0300

    feat: add a header

--- frontend/invoice-generator/src/pages/LandingPage/LandingPage.jsx
+++ frontend/invoice-generator/src/pages/LandingPage/LandingPage.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Header from "../../components/landing/Header";
 
 const LandingPage = () => {
-  return <div>LandingPage</div>;
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [profileDropdownOpen, setProfileDropdownOpen] = useState(false);
+
+  const isAuthenticated = false;
+  const user = { name: "Natnael", email: "natitam@exmple.com" };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 10);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <div className="bg-[#ffffff] text-gray-600">
+      <Header />
+    </div>
+  );
 };
 
 export default LandingPage;
