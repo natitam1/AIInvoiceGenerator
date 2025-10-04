a64b3322a4fc734499c8a0d9d999312ba2ec7b42
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-10-04 21:09:56 +0300

    feat: add landing page for it

--- frontend/invoice-generator/src/pages/LandingPage/LandingPage.jsx
+++ frontend/invoice-generator/src/pages/LandingPage/LandingPage.jsx
@@ -2,20 +2,6 @@ import React, { useEffect, useState } from "react";
 import Header from "../../components/landing/Header";
 
 const LandingPage = () => {
-  const [isScrolled, setIsScrolled] = useState(false);
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [profileDropdownOpen, setProfileDropdownOpen] = useState(false);
-
-  const isAuthenticated = false;
-  const user = { name: "Natnael", email: "natitam@exmple.com" };
-
-  useEffect(() => {
-    const handleScroll = () => {
-      setIsScrolled(window.scrollY > 10);
-    };
-    window.addEventListener("scroll", handleScroll);
-    return window.removeEventListener("scroll", handleScroll);
-  }, []);
 
   return (
     <div className="bg-[#ffffff] text-gray-600">
